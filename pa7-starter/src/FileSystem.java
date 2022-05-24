35d85407aad0103eb7f089c051bfcd4d5811e9ee
YoavGutmanUCSD <ygutman@ucsd.edu>
2022-05-24 16:58:54 -0700

    Constructor, add, and decideAction first draft

--- pa7-starter/src/FileSystem.java
+++ pa7-starter/src/FileSystem.java
@@ -14,31 +14,74 @@ public class FileSystem {
     
     // TODO
     public FileSystem() {
-
+        this.nameTree = new BST<String, FileData>();
+        this.dateTree = new BST<String, ArrayList<FileData>>();
     }
 
 
     // TODO
     public FileSystem(String inputFile) {
     	// Add your code here
+        this.nameTree = new BST<String, FileData>();
+        this.dateTree = new BST<String, ArrayList<FileData>>();
         try {
             File f = new File(inputFile);
             Scanner sc = new Scanner(f);
             while (sc.hasNextLine()) {
                 String[] data = sc.nextLine().split(", ");
                 // Add your code here
+                String fileName = data[0];
+                String fileDir = data[1];
+                String fileDate = data[2];
+                add(fileName, fileDir, fileDate); // make this work when you can 
+                // FileData fileOnLine = new FileData(fileName, fileDir, fileDate);
+                // nameTree.put(fileName, fileOnLine);
+                // ArrayList<FileData> dateList = dateTree.get(fileDate);
+                // if(dateList == null){
+                //     dateList = new ArrayList<FileData>();
+                //     dateList.add(fileOnLine);
+                //     dateTree.put(fileName, dateList);
+                // }
+                // else {
+                //     dateList.add(fileOnLine);
+                // }
             }
             sc.close();
         } catch (FileNotFoundException e) {
             System.out.println(e);
-
         }
     }
 
 
     // TODO
     public void add(String name, String dir, String date) {
-    	
+        FileData file = new FileData(name, dir, date);
+        nameTree.put(name, file);
+        ArrayList<FileData> dateList = dateTree.get(date);
+        if(dateList == null){
+            dateList = new ArrayList<FileData>();
+            dateList.add(file);
+            dateTree.put(name, dateList);
+        }
+        else {
+            dateList.add(file);
+        }
+    }
+    private int decideAction(String name, String dir, String date){
+        // return 0 if add, 1 if modify, 2 if do nothing
+        if(name == null || dir == null || date == null){
+            return 2;
+        }
+        FileData fileInSystem = nameTree.get(name);
+        if(name.equals(fileInSystem.name)){ // if naem is not the same, add it no matter what
+            return 0;
+        }
+        if(date.equals(fileInSystem.lastModifiedDate)){
+            return 2;
+        }
+        else {
+            return 1;
+        }
     }
 
 
