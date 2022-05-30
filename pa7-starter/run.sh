76c6da64bc5e372cfb876adfed025717272c279e
YoavGutmanUCSD <ygutman@ucsd.edu>
2022-05-30 12:14:32 -0700

    No syntax errors now hopefully

--- pa7-starter/run.sh
+++ pa7-starter/run.sh
@@ -1,10 +1,10 @@
 git pull
 classpath="src/:lib/hamcrest-core-1.3.jar:lib/junit-4.12.jar:classes/"
 
-run_to_file (files, toRun) {
-    echo "Compiling for and running ${toRun}..."
-    javac -cp $classpath -d classes/ src/${files}.java > ${toRun}_compile.log 2>&1
-    java -cp $classpath org.junit.runner.JUnitCore ${toRun} > ${toRun}_output.log 2>&1
+run_to_file () {
+    echo "Compiling for and running ${2}..."
+    javac -cp $classpath -d classes/ src/${1}.java > ${2}_compile.log 2>&1
+    java -cp $classpath org.junit.runner.JUnitCore ${2} > ${2}_output.log 2>&1
 }
 
 filesystem="src/FileData.java src/FileSystem.java"
