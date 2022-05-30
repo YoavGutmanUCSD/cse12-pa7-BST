4de0c0e675f3277ecf67336dc0aefd5beb050798
YoavGutmanUCSD <ygutman@ucsd.edu>
2022-05-30 12:24:52 -0700

    working in the dark tbh

--- pa7-starter/run.sh
+++ pa7-starter/run.sh
@@ -3,14 +3,14 @@ classpath="src/:lib/hamcrest-core-1.3.jar:lib/junit-4.12.jar:classes/"
 
 run_to_file () {
     echo "Compiling for and running ${2}..."
-    javac -cp $classpath -d classes/ ${1} > ${2}_compile.log 2>&1
-    java -cp $classpath org.junit.runner.JUnitCore ${2} > ${2}_output.log 2>&1
+    javac -cp $classpath -d classes/ "${1}" > ${2}_compile.log 2>&1
+    java -cp $classpath org.junit.runner.JUnitCore "${2}" > ${2}_output.log 2>&1
 }
 
 filesystem="src/FileData.java src/FileSystem.java"
 filesystem_test="src/FileSystemTest.java"
 
-bst="src/BST.java src/DefaultMap"
+bst="src/BST.java src/DefaultMap.java"
 bst_test="src/BSTTest.java"
 run_to_file "${bst} ${bst_test}" "BSTTest"
 run_to_file "${filesystem} ${bst} ${filesystem_test}" "FileSystemTest"
