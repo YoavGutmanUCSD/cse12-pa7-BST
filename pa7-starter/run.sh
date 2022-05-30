6090c47f2c04cb9125c4f00742de173fff597532
YoavGutmanUCSD <ygutman@ucsd.edu>
2022-05-30 12:25:32 -0700

    bad change revert

--- pa7-starter/run.sh
+++ pa7-starter/run.sh
@@ -3,8 +3,8 @@ classpath="src/:lib/hamcrest-core-1.3.jar:lib/junit-4.12.jar:classes/"
 
 run_to_file () {
     echo "Compiling for and running ${2}..."
-    javac -cp $classpath -d classes/ "${1}" > ${2}_compile.log 2>&1
-    java -cp $classpath org.junit.runner.JUnitCore "${2}" > ${2}_output.log 2>&1
+    javac -cp $classpath -d classes/ ${1} > ${2}_compile.log 2>&1
+    java -cp $classpath org.junit.runner.JUnitCore ${2} > ${2}_output.log 2>&1
 }
 
 filesystem="src/FileData.java src/FileSystem.java"
