067785d0285ad41a3aed8a9f98a2162b45da06ee
Kathegnosis <g8keren@gmail.com>
2022-06-03 15:16:47 -0700

    death

--- pa7-starter/src/FileSystem.java
+++ pa7-starter/src/FileSystem.java
@@ -59,11 +59,13 @@ public class FileSystem {
             if(fileByDate == null){
                 ArrayList<FileData> fileArrayToAdd = new ArrayList<FileData>();
                 fileArrayToAdd.add(fileToAdd);
+                // System.out.println(date);
                 //dateTree.put(date, fileArrayToAdd);
                 return;
             }
             fileByDate.add(fileToAdd);
             dateTree.set(date, fileByDate);
+            // System.out.println(dateTree.get(date));
         }
         else if (!fileByName.lastModifiedDate.equals(fileToAdd.lastModifiedDate)) {
             nameTree.replace(name, fileToAdd);
@@ -214,90 +216,90 @@ public class FileSystem {
 
     // TODO
     public FileSystem filter(String startDate, String endDate) {
-        return null;
-        // FileSystem newFileSystem = new FileSystem();
-        // String currentDate;
-        // int[] endDateInt = parseDate(endDate);
-        // int[] currentDateInt = parseDate(startDate);
+        // return null;
+        FileSystem newFileSystem = new FileSystem();
+        String currentDate;
+        int[] endDateInt = parseDate(endDate);
+        int[] currentDateInt = parseDate(startDate);
 
-        // while(!generateDateString(currentDateInt).equals(endDate)){
+        while(!generateDateString(currentDateInt).equals(endDate)){
 
-        //     if(currentDateInt.equals(parseDate("2025-01-01"))){
-        //         System.out.println("You still have an infinite loop 4head");
-        //         break;
-        //     }
+            if(currentDateInt.equals(parseDate("2025-01-01"))){
+                System.out.println("You still have an infinite loop 4head");
+                break;
+            }
 
 
-        //     System.out.println(generateDateString(currentDateInt));
-        //     currentDate = generateDateString(currentDateInt);
-        //     ArrayList<FileData> allFilesOnDate = dateTree.get(currentDate);
-        //     if(allFilesOnDate != null) {
-        //         for(int i = 0; i < allFilesOnDate.size(); i++){
-        //             FileData currentData = allFilesOnDate.get(i);
-        //             newFileSystem.add(currentData.name, currentData.dir, currentData.lastModifiedDate);
-        //         }
-        //     }
-        //     currentDateInt = incrementParsedDate(currentDateInt);
-        // }
-        // return newFileSystem;
+            System.out.println(generateDateString(currentDateInt));
+            currentDate = generateDateString(currentDateInt);
+            ArrayList<FileData> allFilesOnDate = dateTree.get(currentDate);
+            if(allFilesOnDate != null) {
+                for(int i = 0; i < allFilesOnDate.size(); i++){
+                    FileData currentData = allFilesOnDate.get(i);
+                    newFileSystem.add(currentData.name, currentData.dir, currentData.lastModifiedDate);
+                }
+            }
+            currentDateInt = incrementParsedDate(currentDateInt);
+        }
+        return newFileSystem;
     }
 
     // convert a string in format YYYY-MM-DD into an int array like [YYYY, MM, DD]
-    // private int[] parseDate(String date){
-    //     String[] dateSplit = date.split("-");
-    //     int[] year_month_day = new int[3];
-    //     for(int i = 0; i < year_month_day.length; i++){
-    //         year_month_day[i] = Integer.valueOf(dateSplit[i]);
-    //     }
-    //     return year_month_day;
-    // }
-    // // this function is irrelevant basically
-    // private String generateDateString(int year, int month, int day){
-    //     return String.format("%s/%s/%s", year, month, day);
-    // }
+    private int[] parseDate(String date){
+        String[] dateSplit = date.split("-");
+        int[] year_month_day = new int[3];
+        for(int i = 0; i < year_month_day.length; i++){
+            year_month_day[i] = Integer.valueOf(dateSplit[i]);
+        }
+        return year_month_day;
+    }
+    // this function is irrelevant basically
+    private String generateDateString(int year, int month, int day){
+        return String.format("%s/%s/%s", year, month, day);
+    }
 
     // reverse parseDate
-    // private String generateDateString(int[] dateInfo){
-    //     // if month is a value under 10, add a 0 so it's 0x instead of x.
-    //     if(dateInfo[1] < 10){
-    //         return String.format("%s-0%s-%s", dateInfo[0], dateInfo[1], dateInfo[2]);
-    //     }
-    //     return String.format("%s-%s-%s", dateInfo[0], dateInfo[1], dateInfo[2]);
-    // }
+    private String generateDateString(int[] dateInfo){
+        // if month is a value under 10, add a 0 so it's 0x instead of x.
+        if(dateInfo[1] < 10){
+            return String.format("%s-0%s-%s", dateInfo[0], dateInfo[1], dateInfo[2]);
+        }
+        return String.format("%s-%s-%s", dateInfo[0], dateInfo[1], dateInfo[2]);
+    }
 
     // increment date. more involved than you might think.
-    // private int[] incrementParsedDate(int[] parsedDate){
-    //     int[] newDate;
-    //     int year = parsedDate[0];
-    //     int month = parsedDate[1];
-    //     int day = parsedDate[2];
-    //     int threshold = daysInMonth(month);
-    //     // if day threshold reached, increment month 
-    //     if (threshold == day){
-    //         // if month threshold reached, increment year and go to january 1st
-    //         if (month == 12){
-    //             return new int[]{year + 1, 1, 1};
-    //         }
-    //         return new int[]{year , month + 1, 1};
-    //     }
-    //     return new int[]{year, month, day+1};
-    // }
+    private int[] incrementParsedDate(int[] parsedDate){
+        int[] newDate;
+        int year = parsedDate[0];
+        int month = parsedDate[1];
+        int day = parsedDate[2];
+        int threshold = daysInMonth(month);
+        // if day threshold reached, increment month 
+        if (threshold == day){
+            // if month threshold reached, increment year and go to january 1st
+            if (month == 12){
+                return new int[]{year + 1, 1, 1};
+            }
+            return new int[]{year , month + 1, 1};
+        }
+        return new int[]{year, month, day+1};
+    }
 
     // some months have 30 days, others have 31, one in particular has 28. 
     // this method will return the proper number of days according to the month
-    // private int daysInMonth(int month){
-    //     int[] monthsWhere31Days = {1, 3, 5, 7, 8, 10, 12};
-    //     int february = 2;
-    //     if (february == month) {
-    //         return 28;
-    //     }
-    //     for(int i = 0; i < monthsWhere31Days.length; i++){
-    //         if(month == monthsWhere31Days[i]){
-    //             return 31;
-    //         }
-    //     }
-    //     return 30;
-    // }
+    private int daysInMonth(int month){
+        int[] monthsWhere31Days = {1, 3, 5, 7, 8, 10, 12};
+        int february = 2;
+        if (february == month) {
+            return 28;
+        }
+        for(int i = 0; i < monthsWhere31Days.length; i++){
+            if(month == monthsWhere31Days[i]){
+                return 31;
+            }
+        }
+        return 30;
+    }
     // // you can use this to account for leap years, but then you'd need to add another argument to daysInMonth
     // private int februaryAccounting(int year){
     //     if(leapYear(year)){
@@ -314,23 +316,23 @@ public class FileSystem {
 
     // TODO
     public FileSystem filter(String wildCard) {
-        return null;
-        // FileSystem newFileSystem = new FileSystem();
-        // List<String> datesToExamine = new LinkedList<String>();
-        // for(String date: dateTree.keys()){
-        //     if(date.contains(wildCard)){
-        //         datesToExamine.add(date);
-        //     }
-        // }
-        // for(String date: datesToExamine){
-        //     List<FileData> filesAtDate = dateTree.get(date);
-        //     for(FileData file: filesAtDate){
-        //         newFileSystem.add(file.name, file.dir, file.lastModifiedDate);
-        //     }
-        // }
-        // // int[] endDateInt = parseDate(endDate);
-        // // int[] currentDateInt = parseDate(startDate);
-        // return newFileSystem;
+        // return null;
+        FileSystem newFileSystem = new FileSystem();
+        List<String> datesToExamine = new LinkedList<String>();
+        for(String date: dateTree.keys()){
+            if(date.contains(wildCard)){
+                datesToExamine.add(date);
+            }
+        }
+        for(String date: datesToExamine){
+            List<FileData> filesAtDate = dateTree.get(date);
+            for(FileData file: filesAtDate){
+                newFileSystem.add(file.name, file.dir, file.lastModifiedDate);
+            }
+        }
+        // int[] endDateInt = parseDate(endDate);
+        // int[] currentDateInt = parseDate(startDate);
+        return newFileSystem;
     }
 
 
